ba5e9e92ee849e0fc2b5986a37237b0395fa9781
EC2 Default User <ec2-user@ip-172-31-28-157.ec2.internal>
2019-09-17 02:05:23 +0000

    Commit for tuition calculator

---
+++ homework3/process_tuition.php
@@ -0,0 +1,14 @@
+<?php
+$residency = $_POST['residency'];
+$number_of_units = $_POST['units'];
+$health_fee = $_POST['healthfee'];
+$college_services_card = $_POST['csc'];
+$parking_permit = $_POST['parking'];
+$submit = $_POST['submit'];
+$
+
+echo"<h1>Cost of tuition: $number_of_units 
+
+
+
+?>
